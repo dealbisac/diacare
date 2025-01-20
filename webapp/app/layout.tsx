8135a115873894bf11b4034a1f641bfdd0e7c2d1
dealbisac <chand.dipendra19@gmail.com>
2025-01-20 13:08:51 +0100

    feat: Add Clerk authentication to webapp layout

--- webapp/app/layout.tsx
+++ webapp/app/layout.tsx
@@ -3,6 +3,13 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,6 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
+    <ClerkProvider>
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
@@ -25,5 +33,6 @@ export default function RootLayout({
         </ThemeProvider>
       </body>
     </html>
+  </ClerkProvider>
   );
 }
