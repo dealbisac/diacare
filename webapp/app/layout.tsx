9870864a28429d8ba1516609e462d82a03ed7f0c
dealbisac <chand.dipendra19@gmail.com>
2025-01-20 13:51:16 +0100

    chore: Update font import in layout.tsx

--- webapp/app/layout.tsx
+++ webapp/app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Raleway } from 'next/font/google';
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
 import {
@@ -11,11 +11,11 @@ import {
   UserButton
 } from '@clerk/nextjs'
 
-const inter = Inter({ subsets: ['latin'] });
+const raleway = Raleway({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'DiaCare - 24/7 real-time support for diabetic patients. ',
-  description: 'A comprehensive platform for diabetes management, support, and information.',
+  description: 'A comprehensive platform for diabetes management, support, and information.'
 };
 
 export default function RootLayout({
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
+      <body className={raleway.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
           <Toaster />
