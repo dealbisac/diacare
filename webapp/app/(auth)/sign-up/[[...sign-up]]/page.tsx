7e2a601c145ed410039dfdfcffa61d87103d9554
dealbisac <chand.dipendra19@gmail.com>
2025-01-21 23:11:42 +0100

    refactor: Update sign-in and sign-up pages with Clerk authentication

---
+++ webapp/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,14 @@
+import { SignUp } from '@clerk/nextjs'
+import Image from 'next/image'
+
+export default function Page() {
+    <div className="grid grid-cols-1 md:grid-cols-2">
+    <div>
+        <Image src={'/diacare.png'} alt='login' width={700} height={1000} className='w-full' />
+
+    </div>
+    <div className="flex justify-center items-center h-screen order-first md:order-last">
+        <SignUp />
+    </div>
+</div>
+}
